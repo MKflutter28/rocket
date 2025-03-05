b43791eae65194d45dabb37ea80118e0decad9ca
Manishkumar28-dev2 <manishnarmala9@gmail.com>
2025-03-05 14:05:50 +0530

    add file

--- lib/main.dart
+++ lib/main.dart
@@ -116,7 +116,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.hail),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
